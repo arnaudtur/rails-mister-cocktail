3328e62c2598c5acdf8ef28fb68e2de7602c65cf
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-13 15:27:49 +0200

    Back End Done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,17 +1,28 @@
 class DosesController < ApplicationController
-    def new
-    @cocktail = Cocktail.find(params[:cocktai_id])
-    @review = Review.new
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
-    @review = Review.new(review_params)
-    # we need `restaurant_id` to associate review with corresponding restaurant
-    @cocktail = Cocktail.find(params[:cocktai_id])
-    @review.restaurant = @cocktail
-    if @review.save
-      redirect_to restaurant_path(@cocktail)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
     else render :new
     end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
