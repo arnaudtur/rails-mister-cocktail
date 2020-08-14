168e76a826a25f0e1224c347a94a9af07d1f7cb6
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-14 13:18:05 +0200

    front time

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
+  has_one_attached :photo
   has_many :doses
   has_many :ingredients, through: :doses, dependent: :destroy
 end
